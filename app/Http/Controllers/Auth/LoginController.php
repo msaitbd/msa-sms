46ebb2aea713e400fe0402d1bf41a7bcf4959c2c
sadekur-rahman <sadekurict@gmail.com>
2024-04-28 17:24:03 +0600

    add:user login type check and goto panel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +38,35 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        $field = 'email';
+        if (filter_var($request->input('email'), FILTER_VALIDATE_EMAIL)) {
+            $field = 'email';
+        } elseif (is_numeric($request->input('email'))) {
+            $field = 'phone';
+        }
+
+        $request->merge([$field => $request->input('email')]);
+        $credentials = $request->only($field, 'password');
+        $remember = request('remember');
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return redirect("login")->with('error',  __('Username or password is incorrect'));
+        }
+
+        if (auth()->check()) {
+            $user = User::where($field, $request->email)->first();
+            if ($user->status == USER_STATUS_ACTIVE && $user->type == USER_TYPE_SUPER_ADMIN) {
+                return view('home');
+            } else {
+                Auth::logout();
+                return redirect("login")->with('error', __('Something Went Wrong'));
+            }
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
