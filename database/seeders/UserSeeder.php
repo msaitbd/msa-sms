de773bb8c9757965255217c0b9e6d5d1d7ee8533
sadekur rahman <sadekurict@gmail.com>
2024-05-05 13:04:38 +0600

    add:seed for admin school

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AdminSchool;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
+        $userData = [
             ['first_name' => 'Super', 'last_name' => 'Admin', 'name' => 'Super Admin', 'email' => 'superadmin@gmail.com', 'phone' => '01234567890', 'type' => USER_TYPE_SUPER_ADMIN, 'status' => USER_STATUS_ACTIVE, 'email_verified_at' => now(), 'password' => Hash::make('password')],
             ['first_name' => 'Admin', 'last_name' => 'School', 'name' => 'Admin School', 'email' => 'admin@gmail.com', 'phone' => '01234567891', 'type' => USER_TYPE_ADMIN, 'status' => USER_STATUS_ACTIVE, 'email_verified_at' => now(), 'password' => Hash::make('password')],
             ['first_name' => 'Teacher', 'last_name' => 'School', 'name' => 'Teacher School', 'email' => 'teacher@gmail.com', 'phone' => '01234567892', 'type' => USER_TYPE_TEACHER, 'status' => USER_STATUS_ACTIVE, 'email_verified_at' => now(), 'password' => Hash::make('password')],
@@ -22,6 +23,16 @@ class UserSeeder extends Seeder
             ['first_name' => 'Student', 'last_name' => 'School', 'name' => 'Student School', 'email' => 'student@gmail.com', 'phone' => '01234567894', 'type' => USER_TYPE_STUDENT, 'status' => USER_STATUS_ACTIVE, 'email_verified_at' => now(), 'password' => Hash::make('password')],
             ['first_name' => 'Parent', 'last_name' => 'School', 'name' => 'Parent School', 'email' => 'parent@gmail.com', 'phone' => '01234567895', 'type' => USER_TYPE_PARENT, 'status' => USER_STATUS_ACTIVE, 'email_verified_at' => now(), 'password' => Hash::make('password')],
         ];
-        User::insert($data);
+        User::insert($userData);
+
+        AdminSchool::create([
+            'admin_id' => 2,
+            'school_name' => 'Msait Institute',
+            'address' => 'Mokhles Plaza, Savar Dhaka',
+            'status' => STATUS_ACTIVE,
+            'service_charge' => 500,
+            'install_fee' => 5000,
+            'created_by' => 1,
+        ]);
     }
 }
